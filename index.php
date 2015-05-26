fd6c33ddddaf3f2944c1328bc5e285cca99f84c9
FiftyStars <fiftystarsdj@gmail.com>
2015-05-26 03:35:28 +0300

    !index update

--- index.php
+++ index.php
@@ -28,10 +28,18 @@ function logTest(){
 
 function localizationTest(){
     $array = [
-        ['name' => 1, 'something' => 2, 'not_translate'=> 'qwerty'],
+        ['name' => 1, 'something' => 2, 'not_translate'=> 'qwerty1'],
         ['name' => 3, 'something' => 4, 'not_translate'=> 'qwerty2'],
         ['name' => 5, 'something' => 6, 'not_translate'=> 'qwerty3'],
-        ['name' => 7, 'something' => 8, 'not_translate'=> 'qwerty4']
+        ['name' => 7, 'something' => 8, 'not_translate'=> 'qwerty4'],
+        ['name' => 9, 'something' => 10, 'not_translate'=> 'qwerty5'],
+        ['name' => 11, 'something' => 12, 'not_translate'=> 'qwerty6'],
+        ['name' => 13, 'something' => 14, 'not_translate'=> 'qwerty7'],
+        ['name' => 15, 'something' => 16, 'not_translate'=> 'qwerty8'],
+        ['name' => 17, 'something' => 18, 'not_translate'=> 'qwerty9'],
+        ['name' => 19, 'something' => 20, 'not_translate'=> 'qwerty10'],
+        ['name' => 21, 'something' => 22, 'not_translate'=> 'qwerty11'],
+        ['name' => 23, 'something' => 24, 'not_translate'=> 'qwerty12']
         
     ];
     localization::replaceIdByTranslationInArrayOfRows($array, ['name', 'something']);
